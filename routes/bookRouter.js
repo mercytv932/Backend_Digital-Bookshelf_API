da0479f090898c03ca654d4741973be3dd669977
Yosef <mercytv932@gmail.com>
2026-09-24 00:33:58 -0400

    Done with router.post in bookRouter

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -13,4 +13,14 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.post("/", async (req, res) => {
+  try {
+    const book = new Book(req.body);
+    const savedBook = await book.save();
+    res.status(201).json(savedBook);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to create book" });
+  }
+});
+
 module.exports = router;
