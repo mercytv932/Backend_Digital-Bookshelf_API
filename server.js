3052d19b63bac55f60a4ee94a5b0c518ac79700d
Yosef <mercytv932@gmail.com>
2026-09-23 23:53:33 -0400

    import, Base URL, Server running

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./db/connection.js");
+const bookRouter = require("./routes/bookRouter.js");
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 dotenv.config();
 connectDB();
+
+app.use(express.json());
+
+app.use("/api/books", bookRouter); //base URL creation
+
+app.listen(PORT, () => {
+  resizeBy.send(`Server running on port http://localhost:${PORT}`);
+});
